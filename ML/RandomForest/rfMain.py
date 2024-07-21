31623fc224933586acf9f6d56ac2a960c6ce4d0c
Nicola <nicoladochnenko2@gmail.com>
2024-07-21 23:35:04 +0100

    added rf files, export metrics to json file

--- ML/RandomForest/rfMain.py
+++ ML/RandomForest/rfMain.py
@@ -1,1 +1,54 @@
-# RF work will be here
+import pandas as pd
+import time
+import json
+from sklearn import preprocessing
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+
+dataset = pd.read_csv("./InSDN/mergedCSV.csv")
+
+X = dataset.drop('Label', axis=1)
+Y = dataset['Label']
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
+
+# normalise data
+
+min_max_scaler = preprocessing.MinMaxScaler(feature_range=(0,1))
+X_train_scaled = min_max_scaler.fit_transform(X_train)
+X_test_scaled = min_max_scaler.transform(X_test)
+
+# fit to classifier
+
+classifier = RandomForestClassifier()
+start = time.time()
+classifier.fit(X_train_scaled, Y_train)
+stop = time.time()
+
+#predictions using metrics
+
+predictions = classifier.predict(X_test_scaled)
+
+accuracy = metrics.accuracy_score(Y_test, predictions)
+recall = metrics.recall_score(Y_test, predictions)
+precision = metrics.precision_score(Y_test, predictions)
+f1_score = metrics.f1_score(Y_test, predictions)
+training_time = stop - start
+
+# write metrics to obj
+
+metrics_obj = {
+    "Accuracy": accuracy,
+    "Recall": recall,
+    "Precision": precision,
+    "F1 Score": f1_score,
+    "Training Time": training_time
+}
+
+# write metrics to file
+
+with open("./ML/RandomForest/metrics.json", "w") as json_file:
+    json.dump(metrics_obj, json_file, indent=4)
+
+print("Metrics written to file in folder")
