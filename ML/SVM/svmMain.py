b5217fb3e65a086f77b984ff1f241664d0231daa
Nicola <nicoladochnenko2@gmail.com>
2024-07-21 15:44:56 +0100

    re-adding ml work

--- ML/SVM/svmMain.py
+++ ML/SVM/svmMain.py
@@ -1,5 +1,10 @@
 from sklearn import svm
 from sklearn import metrics
+from sklearn.model_selection import train_test_split
+
+# Assuming 'X' is your input features and 'y' is your target variable
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
+
 
 classifier = svm.SVC(kernel="linear")
 classifier.fit(X_train, Y_train)
