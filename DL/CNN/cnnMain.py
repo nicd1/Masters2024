334272671de340b915ca743bf4ac5fadcd08cd95
Nicola <nicoladochnenko2@gmail.com>
2024-07-23 19:33:07 +0100

    attempting to reshape data

--- DL/CNN/cnnMain.py
+++ DL/CNN/cnnMain.py
@@ -27,11 +27,14 @@ min_max_scaler = preprocessing.MinMaxScaler(feature_range=(0,1))
 X_train_scaled = min_max_scaler.fit_transform(X_train)
 X_test_scaled = min_max_scaler.transform(X_test)
 
+X_train_shaped = X_train_scaled.reshape((X_train_scaled.shape[0], 1, X_train_scaled.shape[1]))
+X_test_shaped = X_test_scaled.reshape((X_test_scaled.shape[0], 1, X_test_scaled.shape[1]))
+
 # build model
 
 def model_build(input_shape):
     model = Sequential()
-    model.add(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(input_shape, 1)))
+    model.add(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(input_shape)))
     model.add(MaxPooling1D(pool_size=2))
 
     model.add(Conv1D(filters=128, kernel_size=3, activation='relu'))
@@ -48,7 +51,7 @@ def model_build(input_shape):
     return model
 
 # re-shape data for cnn
-input_shape = (X_train_scaled.shape[1],)
+input_shape = (1, X_train_scaled.shape[1],)
 
 # compile model
 
@@ -71,10 +74,10 @@ class training_time_callback(Callback):
 # train model
 
 cb = training_time_callback()
-cnn_model.fit(x=X_train_scaled, y=Y_train, epochs=10, validation_data=(X_test_scaled, Y_test), callbacks=[cb])
+cnn_model.fit(x=X_train_shaped, y=Y_train, epochs=10, validation_data=(X_test_shaped, Y_test), callbacks=[cb])
 
 
-loss, accuracy, recall, precision, f1_score = cnn_model.evaluate(X_test_scaled, Y_test)
+loss, accuracy, recall, precision, f1_score = cnn_model.evaluate(X_test_shaped, Y_test)
 training_time = sum(cb.times)
 
 metrics_obj = {
